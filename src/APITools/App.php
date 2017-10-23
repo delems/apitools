7b5d3caf2c157c59b3ed171155c5f42538f08681
Sergey Nenshaev <sn@mov.ru>
2017-10-23 12:07:38 +0300

    some errors

--- src/APITools/App.php
+++ src/APITools/App.php
@@ -6,11 +6,13 @@ use Pimple\Container;
 use Bullet\Request;
 use Bullet\Response;
 use APITools\Config;
-use APITools\DB;
+use APITools\Exceptions\LogicException;
 
 
-class App extends Container
+class App extends \Bullet\App
 {
+    protected $modules=null;
+
     /**
      * New App instance
      *
@@ -19,17 +21,121 @@ class App extends Container
     public function __construct(array $values = array())
     {
         parent::__construct($values);
+
+        //print_r($values);
+        $this->setHandlers();
     }
 
-    public function run($request) {
-        return ['status'=>true] ;
+    public function getModule($module) {
+        if( isset($this->module[$module])  ){
+            return $this->module[$module];
+        }
+
+        throw new LogicException(507, 500);
     }
 
-    public function sendMail ($from, $to, $subject, $message){
 
-            mail($to, $subject, $message,
-                implode("\r\n", ["From: $from"]));
+    public function setHandlers() {
 
+        $API = $this;
+
+        // Чего-то не работает
+        $this->on('before', function(Request $request, Response $response) {
+            $this->prerun($request);
+        });
+
+
+        $this->on('Exception', function(Request $request, Response $response, \Exception $e) {
+
+            $response->status($e->getCode());
+
+            //print_r($e);
+
+            // Код возврата
+            $ret = ['status' => false ];
+            if( isset($e->getErrorCode) && is_callable($e->getErrorCode)) {
+                $ret['error_code'] = $e->getErrorCode();
+            }else{
+                $ret['error_code'] = $e->getCode();
+            }
+
+            if( $this->config['debug'] ) {
+                $ret['debug'] = $e->getMessage();
+            }
+
+
+            if($e instanceof \APITools\Exceptions\LogicException) {
+
+                $ret['error_text'] = $e->getErrorText();
+                $errors = $e->getErrors();
+
+                if( null !== $errors) {
+                    $ret['errors'] = $errors;
+                }
+
+            }else{
+
+                // Отправка уведомления об ошибке
+                $from = \APITools\Config::getData('Notification.from_name') .  
+                        '<' . \APITools\Config::getData('Notification.from_mail') . '>';
+
+                $to = \APITools\Config::getData('Notification.to_fail_mail');
+                $subject = 'APIv1: Fail event';
+                $message = $e->getMessage() . "\n".
+                    //"\n\nQuery:\n". $qdata . 
+                    //"\n\nParams:\n". $pdata .
+                    "\n\nServer:\n". print_r($_SERVER,1);
+
+                mail($to, $subject, $message,
+                    implode("\r\n", ["From: $from"]));
+
+
+            }
+
+            $response->content($ret);
+
+        });
+
+        $this->removeResponseHandler('array_json');
+        $this->registerResponseHandler(
+            null,
+            function($response) {
+                $response->contentType('application/json');
+                $content = $response->content();
+                if( !isset($content['status']) ) {
+                    $content=['status'=>true, 'count'=>count($content), 'data'=>$content];
+                }
+            
+                $response->content(json_encode($content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT ));
+            }
+        );
+   
+    }
+
+
+    /* 
+        Все до начала обработки 
+    */
+    public function prerun(Request $request) {
+
+        $raw = $request->raw();
+
+        if( $raw !== NULL and $raw != '') {
+            $json = json_decode($raw, true);
+
+            if($json !== NULL) {
+                
+                $request->setParams($json);
+            }else{
+                $ret = [
+                    'status' => false,
+                    'error_code' => 102,
+                    'error_text' => 'POST-параметры должны передаваться в формате JSON',
+                ];
+                echo json_encode($ret, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT );
+                exit;
+            }
+        }
     }
 
 }
--- src/APITools/Exceptions/LogicException.php
+++ src/APITools/Exceptions/LogicException.php
@@ -4,5 +4,109 @@ namespace APITools\Exceptions;
 
 class LogicException extends \Exception
 {
+    protected $codes = [
+        100 => 'Неизвестная ошибка',
+        101 => 'Ошибка авторизации',
+        102 => 'POST-параметры должны передаваться в формате JSON',
+        103 => 'Требуется авторизация',
+        104 => 'Запрашиваемый метод API не найден',
+        105 => 'Email не подтвержден',
+        106 => 'Неверный код подтверждения',
+        107 => 'Учетная запись уже активирована',
+        108 => 'Учетная запись не обнаружена',
+        109 => 'Учетная запись не активирована',
+
+        201 =>  'Ошибка заполнения полей',
+        209 =>  'Отсутствует обязательное поле',
+        210 =>  'Поле не может быть пустым',
+        211 =>  'Неверный формат числового поля',
+        212 =>  'Неверный формат булевого поля',
+        214 =>  'Неверный формат поля с плавающей точкой',
+
+        215 =>  'Неверный формат поля email',
+        216 =>  'Неверный формат поля телефон',
+        217 =>  'Неверный формат поля email или телефон',
+        219 =>  'Уже зарегистрирован',
+
+        220 =>  'Неверное значение поля',
+        221 =>  'Неверный формат даты',
+
+        301 =>  'Запись не нейдена',
+
+
+        500 => 'Service Unavailable',
+        501 => 'Не определены модули',
+        502 => 'Не определены поля модуля',
+        503 => 'Не определена таблица с данными',
+        504 => 'Не определено поле таблицы',
+        505 => 'Не определена реакция action',
+        506 => 'Пустой запрос where к БД',
+        507 => 'Модуль не определен',
+        508 => 'Ошибка при добавлении данных',
+        509 => 'Ошибка доступа к данным',
+        510 => 'Поле недопустимо для поиска',
+        511 => 'Ошибка при удалении данных',
+        512 => 'Ошибка параметров',
+        513 => 'Ошибка проверки параметров по action',
+        514 => 'Ошибка при сохранении данных',
+        515 => 'Ошибка при обновлении данных',
+        516 => 'Не определено поле key',
+        517 => 'Нет вариантов enum',
+        518 => 'Отсутствует ключевое поле',
+        519 => 'Не определно поле where',
+        520 => 'Отсутствует поле',
+    ];
+
+    protected $subcode = null;
+    protected $subtext = null;
+    protected $errors = null;
+
+    public function __construct($message = null, $code = 0, Exception $previous = null){
+
+        if( is_array($message) ) {
+            $this->subcode=$message[0];
+            $this->subtext=$message[1];
+            $message=$this->subtext;
+        }else{
+            if( $code !== 500) {
+                $code = 500;
+            }
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getErrorCode() {
+        return $this->subcode;
+    }
+
+    public function getErrorText($code = null) {
+
+        if( $code === null ) {
+            if( $this->subtext ) {
+                return $this->subtext;
+            }
+
+            $code = $this->getMessage();
+        }
+
+        if( isset($this->codes[$code]) ) {
+            return $this->codes[$code];
+        }
+
+        return $code;
+    }
+
+    public function setErrors(array $errors) {
+        //print_r($errors);
+        foreach ($errors as $value) {
+            $value['error_text'] = $this->getErrorText( $value['error_code']);
+            $this->errors[] = $value;
+        }
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
 
 }
